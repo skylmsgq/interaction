bfd55b6c699b60abf8abcbc048aa611776268c7c
Zhang Bin <sjtuzb@gmail.com>
2014-10-13 08:55:09 +0800

    update wechat

--- wechat/index.php
+++ wechat/index.php
@@ -5,11 +5,48 @@ define("TOKEN", "omnilab");
 date_default_timezone_set(PRC);
 
 $wechatObj = new wechatCallbackapiTest();
+//$wechatObj->valid();
 $wechatObj->responseMsg();
 
 
 class wechatCallbackapiTest
 {   
+    public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        //valid signature , option
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
+        }
+    }
+
+    private function checkSignature()
+    {
+        // you must define TOKEN by yourself
+        if (!defined("TOKEN")) {
+            throw new Exception('TOKEN is not defined!');
+        }
+        
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+                
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        // use SORT_STRING rule
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public function responseMsg()
     {
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
