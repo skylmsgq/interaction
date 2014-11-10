e38aed4fa6e0a8f71e8d4c24dc7ad3df8d487bad
Honlan <493722771@qq.com>
2014-11-10 19:15:46 +0800

    modify demo

--- wechat/demo/air.php
+++ wechat/demo/air.php
@@ -1,28 +1,30 @@
 <?php
-$post_string = '%7B%22limit%22%3A%201%2C%20'.'%22sort%22%3A%20%22_id%20desc%22%2C%20'.'%22resource_id%22%3A%20%22d97626a1-6b41-45f5-8f3f-7420f9d60d3c%22%7D';
+$post_string = '%7B%22limit%22%3A%201%2C%20'.'%22sort%22%3A%20%22_id%20desc%22%2C%20'.'%22resource_id%22%3A%20%22350ae6a4-0de0-4c93-a433-3d49fda92662%22%7D';
 $remote_server = 'http://202.121.178.242/api/3/action/datastore_search';
 $context = array(
 	'http'=>array(
 		'method'=>'POST',
-		'header'=>'Authorization: 954c00c0-b01a-4863-a75b-1ed238d38f35',
+		'header'=>'Authorization: a6c2ce2d-9e11-4be0-9ffb-ffe4966ed9e2',
 		'content'=>$post_string )
 );
 $stream_context = stream_context_create( $context );
 $data = json_decode( file_get_contents( $remote_server, FALSE, $stream_context ), true );
 $result = $data['result']['records'];
 $pm = $result[0]['pm2.5'];
+$humidity = $result[0]['humidity'];
 
-$post_string = '%7B%22limit%22%3A%201%2C%20'.'%22sort%22%3A%20%22_id%20desc%22%2C%20'.'%22resource_id%22%3A%20%22519e34eb-920d-4215-a634-a47832e03cf6%22%7D';
-$context = array(
-	'http'=>array(
-		'method'=>'POST',
-		'header'=>'Authorization: 954c00c0-b01a-4863-a75b-1ed238d38f35',
-		'content'=>$post_string )
-);
-$stream_context = stream_context_create( $context );
-$data = json_decode( file_get_contents( $remote_server, FALSE, $stream_context ), true );
-$result = $data['result']['records'];
-$humidity = $result[0]['value'];
+// $post_string = '%7B%22limit%22%3A%201%2C%20'.'%22sort%22%3A%20%22_id%20desc%22%2C%20'.'%22resource_id%22%3A%20%22519e34eb-920d-4215-a634-a47832e03cf6%22%7D';
+// $context = array(
+// 	'http'=>array(
+// 		'method'=>'POST',
+// 		'header'=>'Authorization: 954c00c0-b01a-4863-a75b-1ed238d38f35',
+// 		'content'=>$post_string )
+// );
+// $stream_context = stream_context_create( $context );
+// $data = json_decode( file_get_contents( $remote_server, FALSE, $stream_context ), true );
+// $result = $data['result']['records'];
+// $humidity = $result[0]['value'];
+
 echo json_encode( array(
 		'pm' => $pm,
 		'humidity' => $humidity )
--- wechat/demo/light.php
+++ wechat/demo/light.php
@@ -1,10 +1,10 @@
 <?php
-$post_string = '%7B%22limit%22%3A%2010000%2C%20%22resource_id%22%3A%20%2203cf1c90-dc2e-44ad-9a6a-e5862fdfd57a%22%7D';
+$post_string = '%7B%22limit%22%3A%2010000%2C%20%22resource_id%22%3A%20%22ed4d2868-d1ca-4aee-9b55-f90252ff2381%22%7D';
 $remote_server = 'http://202.121.178.242/api/3/action/datastore_search';
 $context = array(
 	'http'=>array(
 		'method'=>'POST',
-		'header'=>'Authorization: 954c00c0-b01a-4863-a75b-1ed238d38f35',
+		'header'=>'Authorization: a6c2ce2d-9e11-4be0-9ffb-ffe4966ed9e2',
 		'content'=>$post_string )
 );
 $stream_context = stream_context_create( $context );
